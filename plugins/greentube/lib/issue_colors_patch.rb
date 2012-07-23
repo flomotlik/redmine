63addf2c5761dc333665de8c05069282480d50f7
Florian Motlik <flomotlik@gmail.com>
2012-07-23 14:55:04 +0200

    Fixing Issue date colors and marking as warning when no estimated_hours and due date set

--- plugins/greentube/lib/issue_colors_patch.rb
+++ plugins/greentube/lib/issue_colors_patch.rb
@@ -13,22 +13,25 @@ module IssueColorsPatch
       #Adding a 8 hours for a full working day, as the due date is counted for as well
       # Dividing by 3600 as business_time_until gives back the time in seconds
       css_classes_without_deadline_colors.tap do |classes|
-        if due_date && estimated_hours
+        classes << ' gt_' + if due_date && estimated_hours
           working_hours_left = Time.now.business_time_until(due_date.to_time+1.day)/3600
           critical = estimated_hours*1.5
-          alert = estimated_hours*2
+          warning = estimated_hours*2
           problematic = estimated_hours*3
           ok = estimated_hours*4
 
-          status = case working_hours_left
-                   when 0..estimated_hours then "overdue"
-                   when estimated_hours..critical then "critical"
-                   when critical..alert then "alert"
-                   when alert..problematic then "problematic"
-                   when problematic..ok then "ok"
-                   else "fine"
-                   end
-          classes << ' ' + status
+          case working_hours_left
+            when 0..estimated_hours then "overdue"
+            when estimated_hours..critical then "critical"
+            when critical..warning then "warning"
+            when warning..problematic then "problematic"
+            when problematic..ok then "ok"
+            else "fine"
+          end
+        elsif due_date
+          Date.today.business_days_until(due_date) <= 5 ?  'warning' : ''
+        else
+          ''
         end
       end
     end
