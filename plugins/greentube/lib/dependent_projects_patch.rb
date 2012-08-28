bdb2e48fc8f4b06815ebc9207292ccea34208156
Florian Motlik <flomotlik@gmail.com>
2012-08-28 15:47:14 +0200

    Fix MyTickets view

--- plugins/greentube/lib/dependent_projects_patch.rb
+++ plugins/greentube/lib/dependent_projects_patch.rb
@@ -8,8 +8,10 @@ module DependentProjectsPatch
 
   module InstanceMethods
     def statement_with_dependent_projects
-      "(#{statement_without_dependent_projects}) OR (issues.id IN (SELECT issues.id FROM issues LEFT OUTER JOIN custom_values ON custom_values.customized_type='Issue' AND issues.id=customized_id
-LEFT OUTER JOIN custom_fields ON custom_values.custom_field_id=custom_fields.id WHERE custom_fields.name='dependent_project' AND custom_values.value = '#{project.id}'))"
+      if project
+        "(#{statement_without_dependent_projects}) OR (issues.id IN (SELECT issues.id FROM issues LEFT OUTER JOIN custom_values ON custom_values.customized_type='Issue' AND issues.id=customized_id
+          LEFT OUTER JOIN custom_fields ON custom_values.custom_field_id=custom_fields.id WHERE custom_fields.name='dependent_project' AND custom_values.value = '#{project.id}'))"
+      end
     end
   end
 end
