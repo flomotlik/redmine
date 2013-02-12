b8da4ca5e0000a628585450aba83ab23d9c1c357
Florian Motlik <flomotlik@gmail.com>
2013-02-12 01:22:21 +0100

    Fix dependent project statement to fix mytickets

--- plugins/greentube/lib/dependent_projects_patch.rb
+++ plugins/greentube/lib/dependent_projects_patch.rb
@@ -8,10 +8,12 @@ module DependentProjectsPatch
 
   module InstanceMethods
     def statement_with_dependent_projects
+      statement = statement_without_dependent_projects
       if project
-        "(#{statement_without_dependent_projects}) OR (issues.id IN (SELECT issues.id FROM issues LEFT OUTER JOIN custom_values ON custom_values.customized_type='Issue' AND issues.id=customized_id
+        statement = "(#{statement}) OR (issues.id IN (SELECT issues.id FROM issues LEFT OUTER JOIN custom_values ON custom_values.customized_type='Issue' AND issues.id=customized_id
           LEFT OUTER JOIN custom_fields ON custom_values.custom_field_id=custom_fields.id WHERE custom_fields.name='dependent_project' AND custom_values.value = '#{project.id}'))"
       end
+      statement
     end
   end
 end
